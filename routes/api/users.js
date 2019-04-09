ef564b9509514bddf9c42acb4f9f7b98d1eb1e81
Unitedideas <unitedideas@gmail.com>
2019-04-09 02:15:59 -0700

    Wired newUser to DB

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,46 +6,52 @@ const bcrypt = require('bcryptjs')
 // Load User model
 const User = require('../../models/User')
 
-// @route   GET /api/users/test
+// @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res)=> res.json({msg: 'Users Works'}))
+router.get('/test', (req, res) => res.json({
+    msg: 'Users Works'
+}))
 
-// @route   GET /api/users/register
+// @route   GET api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
-    User.findOne({email: req.body.email})
+    User.findOne({
+            email: req.body.email
+        })
         .then(user => {
-            if(user){
-                return res.status(400).json({email: 'Email already exists'})
+            if (user) {
+                return res.status(400).json({
+                    email: 'Email already exists'
+                })
             } else {
-                const avater = gravatar.url(req.body.email, {
+                const avatar = gravatar.url(req.body.email, {
                     s: '200',   // Size
                     r: 'pg',    // Rating
                     d: 'mm'     // Default
                 })
+
                 const newUser = new User({
                     name: req.body.name,
                     email: req.body.email,
-                    avatar: avater,
-                    password: req.body.password,
-
+                    avatar,
+                    password: req.body.password
                 })
-                bcrypt.genSalt(10), (err, salt)=>{
-                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if(err) throw err
                         newUser.password = hash
-                        newUser.save()
+                        newUser
+                            .save()
                             .then(user => res.json(user))
-                            .catch (err => console.log(err))
+                            .catch(err => console.log(err))
                     })
-                }
+                })
             }
         })
         .catch(err => console.log(err))
 })
 
 module.exports = router
-
-
