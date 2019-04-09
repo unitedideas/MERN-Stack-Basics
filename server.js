bd5039603ad2e99e0774907b67fcca0dcbfed475
Unitedideas <unitedideas@gmail.com>
2019-04-08 23:53:32 -0700

    Wire Routes

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
-const express = require('express')();
+const express = require('express')()
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+
+const users = require('./routes/api/users')
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
 
 const app = express;
 
 // DB config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI
 
 // Conect to MongoDB
 mongoose
     .connect(db)
     .then(()=> console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World'))
 
-const port = process.env.PORT || 5000;
+// Use routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
-app.listen(port, ()=> console.log(`Server running on port ${port}`));
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> console.log(`Server running on port ${port}`))
